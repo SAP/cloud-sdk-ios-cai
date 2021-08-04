9a1027bd5f1ffb8436f5018c7a5c35c4df732852
Marco Eidinger <marco.eidinger@sap.com>
2021-08-04 16:44:34 -0700

    fix: 🐛 showing the first 11 Quick replies only
    previously all the Quick reply buttons were visible. However, UX
    guidelines for Web Client limits the number of visible Quick Replies.
    Hence, restricting number of visible Quick replies to 11 to be
    consistent
    
    JIRA: SAPMLCONV-22301

--- Sources/SAPCAI/UI/MessageView/QuickRepliesMessageView.swift
+++ Sources/SAPCAI/UI/MessageView/QuickRepliesMessageView.swift
@@ -18,6 +18,12 @@ struct QuickRepliesMessageView: View {
         assertionFailure("only use `QuickRepliesMessageView` with message type `quickReplies`")
         return nil
     }
+
+    var amountOfButtons: Int {
+        let limitForVisibleButtons = 11
+        guard let amountOfButtons = buttons?.buttonsData?.count else { return 0 }
+        return (amountOfButtons >= limitForVisibleButtons) ? limitForVisibleButtons : amountOfButtons
+    }
     
     private var padding: CGFloat {
         self.themeManager.value(for: .containerLTPadding, type: CGFloat.self, defaultValue: 0)
@@ -25,13 +31,13 @@ struct QuickRepliesMessageView: View {
     
     var body: some View {
         VStack(alignment: .leading, spacing: vSizeClass == .compact ? 2 : 4) {
-            BotTextView(value: NSAttributedString(string: buttons!.buttonText!), isMarkdown: false, geometry: geometry)
+            BotTextView(value: NSAttributedString(string: buttons?.buttonText ?? ""), isMarkdown: false, geometry: geometry)
 
-            if buttons!.buttonsData != nil {
+            if let buttons = buttons, let buttonsData = buttons.buttonsData {
                 ScrollView(.horizontal, showsIndicators: false) {
                     HStack(alignment: .center, spacing: 8) {
-                        ForEach(self.buttons!.buttonsData!, id: \.id) { button in
-                            QuickReplyButtonView(button: button)
+                        ForEach(0 ..< amountOfButtons) { i in
+                            QuickReplyButtonView(button: buttonsData[i])
                         }
                     }
                     .padding([.top, .bottom], 1)
@@ -45,9 +51,17 @@ struct QuickRepliesMessageView: View {
 
 #if DEBUG
     struct QuickRepliesMessageView_Previews: PreviewProvider {
+        static func testData(amountOfButtons: Int) -> CAIResponseMessageData {
+            var buttons: [UIModelDataAction] = []
+            for i in 1 ... amountOfButtons {
+                buttons.append(UIModelDataAction("\(i)", "\(i)", .text))
+            }
+            return CAIResponseMessageData(text: "Show up to limitForVisibleButtons", buttons, buttonType: .quickReplies)
+        }
+
         static var previews: some View {
             GeometryReader { geometry in
-                QuickRepliesMessageView(model: testData.model[0], geometry: geometry)
+                QuickRepliesMessageView(model: QuickRepliesMessageView_Previews.testData(amountOfButtons: 15), geometry: geometry).environmentObject(ThemeManager.shared)
             }
         }
     }
