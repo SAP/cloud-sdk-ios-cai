2744ffca468fa5804b9feb1252659a0aeadb6f49
Marco Eidinger <marco.eidinger@sap.com>
2021-08-06 12:26:31 -0700

    fix: 🐛 icon color appropiate to color scheme (dark/light)

--- Sources/SAPCAI/UI/Common/UIKit/IconImageView.swift
+++ Sources/SAPCAI/UI/Common/UIKit/IconImageView.swift
@@ -6,10 +6,28 @@ struct IconImageView: UIViewRepresentable {
     
     var iconColor: UIColor?
 
+    @Environment(\.colorScheme) var colorScheme
+
+    private var iconColorForColorScheme: UIColor {
+        self.colorScheme == .dark ? UIColor.white : UIColor.black
+    }
+
     func makeUIView(context: Context) -> UIImageView {
-        let img = UIImage.fromIconUrl(url: self.iconUrl, size: self.iconSize, color: self.iconColor)
+        let img = UIImage.fromIconUrl(url: self.iconUrl, size: self.iconSize, color: self.iconColor ?? self.iconColorForColorScheme)
         return UIImageView(image: img)
     }
 
     func updateUIView(_ uiView: UIImageView, context: Context) {}
 }
+
+#if DEBUG
+    struct IconImageView_Previews: PreviewProvider {
+        static var previews: some View {
+            ForEach(ColorScheme.allCases, id: \.self) {
+                ImageView(imageUrl: URL(string: "sap-icon://accept")!)
+                    .preferredColorScheme($0)
+                    .previewLayout(.fixed(width: 200, height: 200))
+            }
+        }
+    }
+#endif
