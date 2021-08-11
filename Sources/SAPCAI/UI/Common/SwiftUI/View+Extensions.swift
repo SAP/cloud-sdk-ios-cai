c6b351e37d71dfba9b4c8a006eeed5562b1a51d6
XiaoYu <33440079+ShadowTourist@users.noreply.github.com>
2021-08-11 10:58:11 +0800

    fix: 🐛 UI improvement for spacing and heights (#33)

--- Sources/SAPCAI/UI/Common/SwiftUI/View+Extensions.swift
+++ Sources/SAPCAI/UI/Common/SwiftUI/View+Extensions.swift
@@ -60,3 +60,14 @@ struct EdgeBorder: Shape {
         return path
     }
 }
+
+struct RoundedCorner: Shape {
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners,
+                                cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
