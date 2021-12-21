04f788125fd50864e6bd7d904dc432a96186c05c
Marco Eidinger <marco.eidinger@sap.com>
2021-12-21 07:31:32 -0800

    fix: 🐛 postback/link value was shown in persistent static menu
    ✅ Closes: #66

--- Sources/SAPCAI/UI/Common/SwiftUI/MenuUnnestedItem.swift
+++ Sources/SAPCAI/UI/Common/SwiftUI/MenuUnnestedItem.swift
@@ -2,29 +2,25 @@ import SwiftUI
 
 struct MenuUnnestedItem: View {
     @EnvironmentObject private var themeManager: ThemeManager
-    
+
     let menuAction: MenuAction
     let iconName: String
     var body: some View {
         HStack(alignment: .top, spacing: 10) {
-            VStack(alignment: .leading, spacing: 6) {
+            VStack(alignment: .leading) {
                 Text(menuAction.menuTitle)
                     .lineLimit(1)
                     .font(.headline)
                     .foregroundColor(themeManager.color(for: .primary1))
-                Text(menuAction.value!)
-                    .lineLimit(1)
-                    .font(.subheadline)
-                    .foregroundColor(themeManager.color(for: .primary2))
+                    .padding([.top, .bottom], 10)
             }
-            .padding([.top, .bottom], 10)
-            
+
             Spacer()
             Image(systemName: iconName)
                 .resizable()
                 .frame(width: 28, height: 28)
                 .foregroundColor(.blue)
-                .padding([.top, .bottom], 20)
+                .padding([.top, .bottom], 10)
         }
     }
 }
